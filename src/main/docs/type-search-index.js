970fa07c546540e5f8cd9b6e6f39895209e525d6
artem <artempolevoj@yandex.ru>
2023-11-18 18:44:22 +0300

    create javadoc

---
+++ src/main/docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.example.calculator","l":"Calculator"},{"p":"org.example","l":"Main"},{"p":"org.example.calculator","l":"Value"}];updateSearchResults();
