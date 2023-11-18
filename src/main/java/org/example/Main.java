26deb42c09140cd7ef364d30e40b96a6d8ef59d7
artem <artempolevoj@yandex.ru>
2023-11-18 17:37:34 +0300

    update Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,57 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
+import org.example.calculator.Calculator;
+import org.example.calculator.Value;
 
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args)  {
+        Value value = new Value();
+        Calculator calculator = new Calculator();
+        List<String> sings = List.of("+", "-", "*", "/", "exit");
+        double a;
+        double b;
+        double result;
+        String sing;
+        String exit;
+        while (true) {
+            System.out.println("-----CALCULATOR-----");
+            System.out.print("Введите первое значение: ");
+                a = value.getNumber();
+            System.out.print("Введите действие или \"exit\" для выхода: ");
+            sing = value.getSing();
+            if (!sings.contains(sing)) {
+                System.out.println("Не верное действие");
+            } else if (sing.equals("exit")) {
+                System.out.println("Выход");
+                System.exit(1);
+            } else {
+                System.out.print("Введите второе значение: ");
+                    b = value.getNumber();
+                switch (sing) {
+                    case "+"->{
+                        result = calculator.sum(a, b);
+                        System.out.printf("a + b = %.2f\n", result);
+                    }
+                    case "-"->{
+                        result = calculator.difference(a, b);
+                        System.out.printf("a - b = %.2f\n", result);
+                    }
+                    case "*"->{
+                        result = calculator.multiply(a, b);
+                        System.out.printf("a * b = %.2f\n", result);
+                    }
+                    case "/"->{
+                        if (b != 0) {
+                            result = calculator.sum(a, b);
+                            System.out.printf("a / b = %.2f\n", result);
+                        } else {
+                            System.out.println("На ноль делить нельзя");
+                        }
+                    }
+                }
+            }
+        }
     }
 }
